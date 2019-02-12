017617fa405b5a39f8ad272b226123f1b974d833
a_olenin <a.olenin@credit.club>
2019-02-12 18:05:00 +0500

    add

--- server.js
+++ server.js
@@ -3,24 +3,10 @@ const http = require('http');
 
 const server = new http.Server();
 
-let counter = 0;
-
 server.on('request', function (request, response) {
    console.log(request.method, url.parse(request.url));
-   console.log(counter);
 
-   response.end('hello')
+   response.end('someday....');
 });
 
 server.listen(8081);
-
-// const Jimp = require('jimp');
-// const path = require('path');
-//
-// const imgPath = path.resolve(__dirname + '/img/paper.jpg');
-//
-// Jimp.read(imgPath, function (err, image) {
-//    if (err) throw new Error('errors in images');
-//
-//    image.rotate(90).write('nei-img.jpg')
-// });
